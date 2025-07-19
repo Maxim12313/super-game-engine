125b72e7e829ab117ade89aa87c0d84a20e5eddd
maxim kim <maximk@umich.edu>
2025-07-18 20:40:07 -0400

    multiple spinnning cubes

--- code/graphics/tests/main.cpp
+++ code/graphics/tests/main.cpp
@@ -72,10 +72,6 @@ int main() {
     shader.set_int("texture0", 0);
     shader.set_int("texture1", 1);
 
-    glm::mat4 model = glm::mat4(1.0f);
-    model =
-        glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-
     glm::mat4 view = glm::mat4(1.0f);
     view = glm::translate(view, glm::vec3(0, 0, -3));
 
@@ -84,11 +80,17 @@ int main() {
         glm::perspective(glm::radians(45.0), window::RATIO, 0.1, 1000.0);
 
     shader.use();
-    shader.set_mat4("model", glm::value_ptr(model));
     shader.set_mat4("view", glm::value_ptr(view));
     shader.set_mat4("projection", glm::value_ptr(projection));
 
     glEnable(GL_DEPTH_TEST);
+
+    glm::vec3 cube_positions[] = {
+        glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f), glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),  glm::vec3(-1.7f, 3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),  glm::vec3(1.5f, 2.0f, -2.5f),
+        glm::vec3(1.5f, 0.2f, -1.5f),   glm::vec3(-1.3f, 1.0f, -1.5f)};
 
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
@@ -102,14 +104,18 @@ int main() {
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture1);
 
-        model =
-            glm::rotate(model, float(glfwGetTime()) / 60 * glm::radians(50.0f),
-                        glm::vec3(0.5, 1.0f, 0.0f));
-        shader.set_mat4("model", glm::value_ptr(model));
-
         shader.use();
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        size_t size = sizeof(cube_positions) / sizeof(glm::vec3);
+        for (size_t i = 0; i < size; i++) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cube_positions[i]);
+            float angle = 20 * i + 5 * glfwGetTime();
+            model = glm::rotate(model, glm::radians(angle),
+                                glm::vec3{1.0, 0.3, 0.5});
+            shader.set_mat4("model", glm::value_ptr(model));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
